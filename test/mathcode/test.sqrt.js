e05da2bc769195682bbdd1ef80c7914d37835499
foxzhong <foxzhong@tencent.com>
2018-03-23 11:45:56 +0800

    cover

---
+++ test/mathcode/test.sqrt.js
@@ -0,0 +1,13 @@
+let chai = require('chai');
+let expect = chai.expect;
+let My = require('../../mathcode/sqrt.js');
+
+describe("sqrt", function() {
+  it("4的平方根应该等于2", function() {
+    expect(My.sqrt(4)).to.equal(2);
+  });
+
+  // it("参数为负值时应该报错", function() {
+  //   expect(function() {My.sqrt(-1);}).to.throw("负值没有平方根");
+  // });
+});
