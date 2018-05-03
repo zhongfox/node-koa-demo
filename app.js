0c0458f686fdc903fe24fc274a5331c6fdb046dc
foxzhong <foxzhong@tencent.com>
2018-05-03 15:52:43 +0800

    babel test

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const Koa = require('koa');
 const app = new Koa();
+const serve = require('koa-static');
 
-app.use(async ctx => {
-  console.log("hello nerd");
-  ctx.body = 'Hello World tag6';
+app.use(async (ctx, next) => {
+  if (ctx.request.path === '/') {
+    console.log("hello nerd");
+    ctx.response.body = `<h1>Hello, world!</h1> <script src="/index.js"></script>`;
+  } else {
+    await next();
+  }
 });
+app.use(serve('public'));
 
 app.listen(3000);
