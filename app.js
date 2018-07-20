79fe1950d016ef44641c7b8c27b85594033fa932
foxzhong <foxzhong@tencent.com>
2018-07-20 17:35:55 +0800

    tag10

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = new Koa();
 const serve = require('koa-static');
 
 app.use(async ctx => {
-  console.log("hello tag10");
-  ctx.body = 'Hello World tag10';
+  console.log("hello tag11");
+  ctx.body = 'Hello World tag11';
 });
 app.use(serve('public'));
 
